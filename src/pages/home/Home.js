3b5e4929cf0853ad26d18d9a6900e9749af46bf7
Yesu <yesupadam2001@gmail.com>
2023-03-07 23:56:28 +0530

    edited and added new images to home page

--- src/pages/home/Home.js
+++ src/pages/home/Home.js
@@ -25,11 +25,11 @@ function Home() {
           <Link to="/projects/ourprojects"><button className='hero_checkout'>Learn more</button></Link>
         </div>
       </section>
-      <section className='home_section com_bg'>
-        <div>
+      <section className='home_section sec_about com_bg'>
+        <div className='home_section_imge'>
           <img src={aboutimg} alt="about illustration"/>
         </div>
-        <div>
+        <div className="home_section_content">
           <h1 className='section_title'>About us</h1>
           <p className='about_para'>
           MAN create & innovative products with unique structure and resilience. We are passionate about our products & service and how they empower our customer's lives. Founded in end of 2021, we grew quickly thanks to our committed contributors and shareholders.
@@ -38,33 +38,33 @@ function Home() {
         </div>
       </section>
       <section className="home_section sec_sustain">
-        <div>
+        <div className="home_section_content">
           <h1 className='section_title'>Sustainability</h1>
           <p>Creating a culture of sustainable creativity.</p>
           <p>The concerns and growing awareness among all stakeholders about global warming and climate change debate poses serious challenges. At MA, we are committed to tackling the issues of Climate Change by adopting Sustainable and responsible growth. To achieve this, we have developed a Sustainability Model.</p>
           <Link to="/sustainability"><button>LEARN MORE</button></Link>
         </div>
-        <div>
+        <div className='home_section_imge'>
           <img src={sustainimg} alt="sustainability illustration"/>
         </div>
       </section>
       <section className="home_section sec_usa com_bg">
-        <div>
+        <div className='home_section_imge'>
           <img src={purposeimg} alt="purpose illustration"/>
         </div>
-        <div>
+        <div className="home_section_content">
           <h1 className='section_title'>Our Purpose in USA</h1>
           <p>Building the electric-vehicle charging infrastructure America needs.</p>
           <Link to="/projects/ourprojects"><button>LEARN MORE</button></Link>
         </div>
       </section>
       <section className="home_section sec_join_us">
-        <div>
+        <div className="home_section_content">
           <h1 className='section_title'>Join Us</h1>
           <p>When you work here, you get that our values are more than just words on paper. You’re joining a place where diversity, equity, and inclusion are at the heart of our business practices and culture.</p>
           <Link to="/careers"><button>Careers</button></Link>
         </div>
-        <div>
+        <div className='home_section_imge'>
           <img src={careerimg} alt="career illustration"/>
         </div>
       </section>
