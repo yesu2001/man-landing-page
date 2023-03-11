8bb5085bd6e128619c66e6498dda2d5638f77320
Yesu <yesupadam2001@gmail.com>
2023-03-12 01:27:48 +0530

    changed invesotrs page,careers page and investors page

--- src/pages/about/About.js
+++ src/pages/about/About.js
@@ -10,6 +10,7 @@ import { TbBuildingFactory2 } from 'react-icons/tb';
 import { HiArrowLongRight } from 'react-icons/hi2';
 import { Link } from 'react-router-dom';
 import './About.css'
+import Management from '../management/Management';
 
 function About() {
   document.title = 'About - MAN.';
@@ -21,25 +22,47 @@ function About() {
       <Navbar/>
       <div className="about_page">
         <div className="about_section">
-          <h2 className='section_title'>Promise To Grow Responsibly</h2>
-          <div className='about_section_content'>
-            <p>MAN is committed to complying with local laws and regulations as well as applying a strict global code of conduct to all employees.</p>
-            <p>It believes that ethical management is not only a tool for responding to the rapid changes in the global business environment, but also for building trust with its various stakeholders including customers, shareholders, employees, business partners and local communities.
-            </p>
-            <p>With an aim to become one of the most responsible companies In the world.</p>
+          <div>
+            <div className='about_section_title'>
+              <h2>MISSION</h2>
+              {/* <h2>I</h2>
+              <h2>S</h2>
+              <h2>S</h2>
+              <h2>I</h2>
+              <h2>O</h2>
+              <h2>N</h2> */}
+            </div>
+            <div className='about_section_content'>
+              <h2 className='about_section_subheading'>Promise To Grow Responsibly</h2>
+              <div className='about_section_para'>
+                <p>MAN is committed to complying with local laws and regulations as well as applying a strict global code of conduct to all employees.</p>
+                <p>It believes that ethical management is not only a tool for responding to the rapid changes in the global business environment, but also for building trust with its various stakeholders including customers, shareholders, employees, business partners and local communities.
+                </p>
+                <p>With an aim to become one of the most responsible companies In the world.</p>
+              </div>
+            </div>
           </div>
         </div>
-        <hr/>
         <div className='about_section'>
-          <h2 className='section_title'>Who we are</h2>
-          <div className="about_section_content">
-            <p>
-            MAN commenced its operations in the year 2022, since then the company has periodically incorporated innovative production technologies and updated its methods to match current global standards ensuring over all optimization of its processes.</p>
-            <p>Our firm is designed to operate as one—a single global corporation united by a strong set of values, including a deep commitment to diversity, and a common purpose: to create positive, enduring change in the world.</p>
+          <div>
+            <div className='about_section_title'>
+              <h2>STORY</h2>
+              {/* <h2>T</h2>
+              <h2>O</h2>
+              <h2>R</h2>
+              <h2>Y</h2> */}
+            </div>
+            <div className='about_section_content'>
+              <h2 className='about_section_subheading'>Who we are</h2>
+              <div className="about_section_para">
+                <p>
+                MAN commenced its operations in the year 2022, since then the company has periodically incorporated innovative production technologies and updated its methods to match current global standards ensuring over all optimization of its processes.</p>
+                <p>Our firm is designed to operate as one—a single global corporation united by a strong set of values, including a deep commitment to diversity, and a common purpose: to create positive, enduring change in the world.</p>
+              </div>
+            </div>
           </div>
         </div>
-        <hr/>
-        <div className='about_section'>
+        {/* <div className='about_section'>
           <h2 className='section_title'>Management committee</h2>
           <div className='about_section_content'>
             <p>Our Directors, officers and employees of MAN, Inc is the corporation's greatest strength.<br/>
@@ -48,11 +71,13 @@ function About() {
   <br/><Link to="/about/management" className="btn">Learn more <HiArrowLongRight/></Link>
   </p>
           </div>
-        </div>
+        </div> */}
         <hr/>
-        <div className='about_section'>
-          <h2 className='section_title'>Our commitment</h2>
-          <div className='about_section_content'>
+        <Management/>
+        <hr/>
+        <div className='about_div'>
+          <h2 className='about_div_header'>Our commitment</h2>
+          <div className='about_section_para'>
             <p>
             We are dedicated to managing our firm and working with our clients in ways that benefit individuals, communities, and the environments they live in.We will reach net-zero climate impact by 2030 through decarbonizing our operations, investing in natural climate solutions, and accelerating progress through partnerships.
   We have a deep and longstanding commitment to advancing diversity, equity, and inclusion in business, in society, and within our firm.
@@ -60,9 +85,9 @@ function About() {
           </div>
         </div>
         <hr/>
-        <div className='about_section'>
-          <h2 className='section_title'>Our guiding principles</h2>
-          <div className='about_section_content'>
+        <div className='about_div'>
+          <h2 className='about_div_header'>Our guiding principles</h2>
+          <div className='about_section_para'>
           <p>We are committed to enhancing the long-term value of the investment dollars entrusted to us by our shareholders. By running the business responsibly, we expect our shareholders to be rewarded with superior returns.
 Success depends on our ability to consistently satisfy ever-changing customer preferences. We commit to being innovative and responsive, while offering high-quality products and services.
 
@@ -72,9 +97,9 @@ We commit to being a good corporate citizen in all the places we operate worldwi
           </div>
         </div>
         <hr/>
-        <div className='about_section'>
-            <h2 className='section_title'>Our Project Commitment</h2>
-            <div className='about_section_content'>
+        <div className='about_div'>
+            <h2 className='about_div_header'>Our Project Commitment</h2>
+            <div className='about_section_para'>
               <p>Affordable and sustainable energy solutions are required to advance global prosperity. We invest in technology and communities to bring the world better energy</p>
               <div className="section_list">
                 <div>
@@ -105,16 +130,16 @@ We commit to being a good corporate citizen in all the places we operate worldwi
             </div>
         </div>
         <hr/>
-        <div className='about_section'>
-            <h2 className='section_title'>Sustainable Growth.</h2>
-            <div className='about_section_content'>
+        <div className='about_div'>
+            <h2 className='about_div_header'>Sustainable Growth.</h2>
+            <div className='about_section_para'>
               <p>We are being a responsible and pioneering business. It goes back to the 2022 days of our founders, who launched one of the purposeful brands for the future. And it is how we run our company today.We strive to do more good for our planet and our society – not just less harm. We want to act on the social and environmental issues facing the world and we want to empower people’s lives with our innovative, sustainable products.This is how we will grow our business.</p>
             </div>
         </div>
         <hr/>
-        <div className='about_section'>
-            <h2 className='section_title'>Connect with us.</h2>
-            <div className="about_section_content">
+        <div className='about_div'>
+            <h2 className='about_div_header'>Connect with us.</h2>
+            <div className="about_section_para">
             <p>Thanks for your interest in MAN. We want to help serve you better and improve your experience with our company.Please note this contact option is only for customers based in the US & EU.If you've already used our standard contact channels and would still like to get in touch, please contact me directly using email.While I'm not able to respond to every email, your comments are always taken seriously and will be shared with the relevant teams.<br/>Kind regards<br/>
             <br/><i>President,</i><br/>MAN, Inc.</p>
             </div>
--- src/pages/management/Management.js
+++ src/pages/management/Management.js
@@ -11,40 +11,49 @@ function Management() {
         window.scrollTo(0,0);
     },[])
 	return (
+		<React.Fragment>
+		<h1>Team</h1>
+
 		<div className="management">
-			<Navbar/>
-			{/* <h1 className='page_title'>MANAGEMENT & FOUNDERS</h1> */}
-			<p>We do everything with our core values of hardwork and trust.</p>
+			{/* <p>We do everything with our core values of hardwork and trust.</p> */}
 			<div className="management_section">
-				<h2 className='section_title'>Mounesh Kumar</h2>
-				<span>President and Co-Founder</span>
-				<p>
-					Mounesh Kumar is the President of MAN and serves on its board of directors.<br/>
-					Prior to starting MAN, MK was the product designer for currently ongoing project Energy’s products.<br/>
-					MK is responsible for all of the company’s Operations, including end-to-end management of MAN’s activities, and service and support in all markets. He also headed MAN’s project Energy division and played a key role in the continued development of product. Then his role is on continued development of strategic reseller and supplier relationships, ensuring flexibility in response to an increasingly demanding marketplace.<br/>
-					MK earned a Bachelor of Technology degree from Indian Institute of Technology Madras.<br/>
-				</p>
-				<div className="social_icons">
-					<Link to="" className='icon'><ImTwitter/></Link>
-					<Link to="" className='icon'><ImLinkedin/></Link>
+				<div>
+					<div>
+						<h2>Mounesh Kumar</h2>
+						<div className="social_icons">
+							<Link to="" className='icon'><ImTwitter size={20}/></Link>
+							<Link to="" className='icon'><ImLinkedin size={20}/></Link>
+						</div>
+					</div>
+					<span>President and Co-Founder</span>
 				</div>
+				<p>
+					Mounesh Kumar is the President of MAN and serves on its board of directors.
+					Prior to starting MAN, MK was the product designer for currently ongoing project Energy’s products.
+					MK is responsible for all of the company’s Operations, including end-to-end management of MAN’s activities, and service and support in all markets. He also headed MAN’s project Energy division and played a key role in the continued development of product. Then his role is on continued development of strategic reseller and supplier relationships, ensuring flexibility in response to an increasingly demanding marketplace.
+					MK earned a Bachelor of Technology degree from Indian Institute of Technology Madras.
+				</p>
 			</div>
 			<div className="management_section">
-				<h2 className='section_title'>Antony S</h2>
-				<span>CEO and Co-Founder</span>
-				<p>
-					Antony SundaraBose (Joe) is MAN’s CEO. Joe oversees the development of UI & software for the product. He is responsible for delivering the software for MAN’s innovative products, including the user interface, applications and frameworks.<br/>
-					Prior to his return, Joe worked at Appstone, where he held several roles including team management.<br/>
-					He took on responsibility for product software as well, delivering all subsequent releases of the advanced UI systems for customers.<br/>
-					Joe holds a Bachelor's in Computer Application from the University of Bangalore, Karnataka, India.<br/>
-				</p>
-				<div className="social_icons">
-					<Link to="" className='icon'><ImTwitter/></Link>
-					<Link to="" className='icon'><ImLinkedin/></Link>
+				<div>
+					<div>
+						<h2>Antony S</h2>
+						<div className="social_icons">
+							<Link to="" className='icon'><ImTwitter size={20}/></Link>
+							<Link to="" className='icon'><ImLinkedin size={20}/></Link>
+						</div>
+					</div>
+					<span>CEO and Co-Founder</span>
 				</div>
+				<p>
+					Antony SundaraBose (Joe) is MAN’s CEO. Joe oversees the development of UI & software for the product. He is responsible for delivering the software for MAN’s innovative products, including the user interface, applications and frameworks.
+					Prior to his return, Joe worked at Appstone, where he held several roles including team management.
+					He took on responsibility for product software as well, delivering all subsequent releases of the advanced UI systems for customers.
+					Joe holds a Bachelor's in Computer Application from the University of Bangalore, Karnataka, India.
+				</p>
 			</div>
-			<FooterSection/>
 		</div>
+		</React.Fragment>
 	)
 }
 
